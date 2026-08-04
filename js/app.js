463a3c63969925dc53cebe3d5b45d8194aa98c4b
Bagus Untung Saputra <bagustegal85@gmail.com>
2026-08-04 13:08:00 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 async function loadBattery() {
     try {
-        const response = await fetch("http://103.174.236.47:14999/api/battery");
+        const response = await fetch("https://103.174.236.47:14999/api/battery");
         const data = await response.json();
 
         // Status
